4271cb67766bb213292a64d6a79e59449cf69238
Param Iyer <viye279@UoA.auckland.ac.nz>
2020-03-13 12:35:28 +1300

    Added comments & summary

--- test_param.R
+++ test_param.R
@@ -1,1 +1,8 @@
+# Loading CSV file
 prjDetPanel_Jan2011 <- read.csv("prjDetPanel-Jan2011.csv")
+# Display summary of all columns
+# Numeric will have 5-point summary + Mean
+# Category will have counts per case
+# Date/Time are being treated as category as it is plain text
+summary(prjDetPanel_Jan2011)
+# End of Job
