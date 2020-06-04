cb12f7dd315fc462793c7d869a26a550dc4efa2b
Param Iyer <viye279@UoA.auckland.ac.nz>
2020-06-04 21:40:34 +1200

    Added extra columns Included PCA

--- test_param.R
+++ test_param.R
@@ -1,7 +1,6 @@
 rm(list = ls(all.names = TRUE))
 
 # Packages
-library(lattice) # xyplot 
 
 # Loading CSV file
 prj <- read.csv("prjDetPanel-Jan2011.csv")
@@ -10,22 +9,80 @@ prj <- read.csv("prjDetPanel-Jan2011.csv")
 # Category will have counts per case
 # Date/Time are being treated as category as it is plain text
 summary(prj)
-# End of Job
 
 # display sample records
 head(prj)
+summary(prj)
 
-# extract 8 cases (64 records)
-prj_8 <- prj[17:80,]
-summary(prj_8$members)
-summary(prj_8)
+# Add sequential observation code
+total_rows <- nrow(prj)
+prj$obsCode <- 1:total_rows
+# Add sequential project code
+prj$prjCode <- (prj$obsCode-1)%/%8+1
+summary(prj)
+
+# Run PCA on dataset
+head(prj[,c(4,7:23,26,27)])
+prcomp(prj[,c(4,7:20,26:27)])
+pca <- prcomp(prj[,c(4,7:20,26:27)])
+pca
+summary(pca)
+prcomp(prj[,c(4,7:20,26,27)], scale.=TRUE)
+pca_scale <- prcomp(prj[,c(4,7:20)], scale.=TRUE)
+pca_scale
+summary(pca_scale)
+
+# extract 10 cases (80 records)
+prj_top_10 <- prj[1:80,]
+summary(prj_top_10)
 
 # plot
-plot(members~Time, data=prj_8)
-xyplot(members~Time|prjId, data=prj_8)
+plot(members~Time, data=prj_top_10)
+library(lattice) # xyplot 
+xyplot(Health~Time|prjId, data=prj_top_10, layout = c(5,2))
 
-ymin<- min(prj_8$members)
-ymax<- max(prj_8$members)
+# extract 10 random cases (80 records)
+prj_rnd_10<-prj[prj$prjCode %in% sample(1:(total_rows/8),10), ]
+summary(prj_rnd_10)
+
+# plot
+plot(members~Time, data=prj_rnd_10)
+library(lattice) # xyplot 
+xyplot(Health~Time|prjId, data=prj_rnd_10, layout = c(5,2))
+
+xyplot(X.~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(prjId~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(Period~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(Time~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(StartDate~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(EndDate~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(forks~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(members~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(commits~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(issues~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(watchers~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(issues~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(pullReq~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(CmtCmnt~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(pullReqCmnt~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+#xyplot(PR.Issue.Cmnt|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(issueCmnt~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(committers~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(MemCommitters~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(PRClosedCnt~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(IssueClosedCnt~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(Health~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(Licence~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(ContribFile~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(OwnerFollower~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(AvgFollower~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(OwnerType~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(obsCode~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+xyplot(prjCode~Time|prjId, data=prj[prj$prjCode %in% sample(1:(total_rows/8),10), ], layout = c(5,2))
+
+
+ymin<- min(prj_8$members)-0.1*(max(prj_8$members)-min(prj_8$members))
+ymax<- max(prj_8$members)+0.1*(max(prj_8$members)-min(prj_8$members))
 
 xyplot(members~Time|prjId, data=prj_8,
        panel=function(x,y){
@@ -47,4 +104,10 @@ boxplot(members ~ Time, data = prj_8)
 boxplot(members ~ OwnerType*Time, data = prj_8)
 boxplot(members ~ OwnerType*Time, data = prj_8,
         names = FALSE, col = c("red", "blue"))
+
+boxplot(members ~ Time, data = prj)
+boxplot(log(members+1) ~ Time, data = prj)
+boxplot(log(log(members+1)+1) ~ Time, data = prj)
+boxplot(log(log(log(members+1)+1)+1) ~ Time, data = prj)
+boxplot(log(log(log(log(members+1)+1)+1)+1) ~ Time, data = prj)
 
